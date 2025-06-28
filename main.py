50ca984fbdad3eee4c2a5a63f5f7e143eba04fd5
Nevo Biton <nevo.biton@weka.io>
2025-06-28 11:10:24 +0300

    adjust main and output to csv

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
 import sentiment
 import models
 import dataset
+import csv
 
 def download_all_models():
     models.download_model(models.gemma_model_name)
     models.download_model(models.mistral_model_name)
-    models.download_model(models.olmo_model_name)
+    models.download_model(models.qwen_model_name)
 
 def cross_check_persona(persona, model, tokenizer, sentiment_estimator):
-    first_person = f"You are {persona}. Describe your thoughts."
-    third_person = f"You know a {persona}. Describe your thoughts."
-    
-    generated = models.inference(tokenizer, model, first_person)[len(first_person):].strip()
-    print("Generated response:", generated)
-    scalar_score, _ = sentiment_estimator.get_sentiment_score(generated)
-    print(f"Sentiment score (0=negative, 1=positive): {scalar_score.item():.4f}")
-
-    generated = models.inference(tokenizer, model, third_person)[len(third_person):].strip()
-    print("Generated response:", generated)
-    scalar_score, _ = sentiment_estimator.get_sentiment_score(generated)
-    print(f"Sentiment score (0=negative, 1=positive): {scalar_score.item():.4f}")
+    results = []
+    questions = dataset.get_persona_questions(persona)
+    for key, question_list in questions.items():
+        for q in question_list[:5]:
+            print(f"Question ({key}): {q}")
+            generated = models.inference(tokenizer, model, q)[len(q):].strip()
+            print("Generated response:", generated)
+            scalar_score, _ = sentiment_estimator.get_sentiment_score(generated)
+            print(f"Sentiment score (0=negative, 1=positive): {scalar_score.item():.4f}")
+            results.append(generated)
+            results.append(scalar_score.item())
+        for q in question_list[5:]:
+            print(f"Question ({key}): {q}")
+            generated = models.inference(tokenizer, model, q)[len(q):].strip()
+            print("Generated response:", generated)
+            results.append(generated)
+    return results
 
 def main():
     sentiment_estimator = sentiment.SentimentScaler()
     tokenizer, model = models.load_model(models.mistral_model_name)
 
-    for i in range(3):
-        persona = dataset.get_next_persona()
-        print('-' * 100)
-        cross_check_persona(persona, model, tokenizer, sentiment_estimator)
-        print('-' * 100)
+    with open('mistral_result.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        headers = ['sex', 'age', 'occupation', 'country', 'marital_status']
+        first_headers = []
+        third_headers = []
+        for i in range(1, 6):
+            first_headers += [f'first_q{i}', f'first_score{i}']
+            third_headers += [f'third_q{i}', f'third_score{i}']
+        for i in range(6,11):
+            first_headers += [f'first_q{i}']
+            third_headers += [f'third_q{i}']
+        headers += first_headers + third_headers
+        writer.writerow(headers)
+
+        while (persona := dataset.get_next_persona()) != None:
+            print('-' * 100)
+            result = cross_check_persona(persona, model, tokenizer, sentiment_estimator)
+            print('-' * 100)
+            writer.writerow(result)
 
 if __name__ == "__main__":
     main()
